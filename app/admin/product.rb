8e95c18485c6b5368cf34b80d6acede250bdbdc3
Denis Sellu <hingasellu@gmail.com>
2015-01-20 11:34:41 +0000

    Products and categories association
    Also  added users and orders association

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,18 +1,19 @@
 ActiveAdmin.register Product do
 
+  permit_params :category_id,:name, :description,:image,:price, :availability, :status
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
+  form do |f|
+    f.inputs do
+      f.input :name, :label => "Product Name"
+      f.input :description, :label => "Description"
+      f.input :image, :label => "Product Image"
+      f.input :price, :label => "Active / Inactive"
+      # f.input :availability, :label => "Active / Inactive"
+      f.input :availability, as: :datepicker, datepicker_options: { min_date: "2013-10-8",        max_date: "+3D" }
+      f.input :status, :label => "Active / Inactive"
+      f.input :category
+    end
 
-
+    f.actions
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ActiveRecord::Base
+  # Make sure dependent is not delete
+  has_many :subcategories, :class_name => "Category", :foreign_key => "parent_id", :dependent => :destroy
+  belongs_to :parent_category, :class_name => "Category",:foreign_key=>"parent_id"
+
+  has_many :products
 end
